d66aed4dc0514c8e5e79d20ba0dea8424af5758d
Crowdbotics <engineering@crowdbotics.com>
2023-08-07 23:15:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-cloud-de-107847",
+  url: "https://weathered-cloud-de-107847.botics.co",
+  api: "https://weathered-cloud-de-107847.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
